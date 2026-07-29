dcb1513ce218daa96a2da6ea1800d57335029e04
immad-khan <immadkhan303@gmail.com>
2026-07-29 12:17:30 +0500

    removed nav bar

--- frontend/Patient-MyProfile.php
+++ frontend/Patient-MyProfile.php
@@ -347,7 +347,6 @@ try {
 </head>
 <body class="theme-cyan">
 <div class="overlay"></div>
-<nav class="navbar p-l-5 p-r-5"><?php include("top_nav.php") ?></nav>
 
 <aside id="leftsidebar" class="sidebar">
     <?php include("patient_sidebar.php") ?>
